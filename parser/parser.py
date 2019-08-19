89edc22c9b6707ec31a007eecf397406179dd645
lrubens <rubenslacouture@gmail.com>
2019-08-19 18:12:09 +0000

    fixed bug in client.py

--- parser/parser.py
+++ parser/parser.py
@@ -2,6 +2,7 @@ from scapy.all import *
 import matplotlib.pyplot as plt
 cipher_list = ["round5_r5n1_1kem_0d", "round5_r5n1_3kem_0d", "round5_r5n1_5kem_0d", "round5_r5nd_1kem_0d", "round5_r5nd_3kem_0d", "round5_r5nd_3kem_5d", "round5_r5nd_5kem_0d", "round5_r5nd_5kem_5d"]
 avgList = list()
+double_fin = False
 for cipher in cipher_list:
     with PcapReader('../results/'+ cipher + '.pcap') as pcap_reader:
         synList = list()
@@ -11,11 +12,18 @@ for cipher in cipher_list:
             if TCP in pkt:
                 if (pkt[TCP].flags & 0x2) and not (pkt[TCP].flags & 0x10):
                     synList.append(pkt.time)
+                    double_fin = False
                 if (pkt[TCP].flags & 0x1) and not (pkt[TCP].flags & 0x8):
-                    finList.append(pkt.time)
+                    if(double_fin):
+                        finList[len(finList) - 1] = pkt.time
+                    else:
+                        finList.append(pkt.time)
+                        double_fin = True
         print(len(finList))
         print(len(synList))
-        for i in range(len(finList)):
+        #print(cipher + 'Synlist:', synList)
+        #print(cipher + 'Finlist:', finList)
+        for i in range(len(synList)):
             sum += finList[i] - synList[i]
         avg = sum/len(synList)
         avgList.append(avg)
--- scripts/client.py
+++ scripts/client.py
@@ -7,7 +7,7 @@ import shutil
 import time
 import pip
 
-num_attempts = 1
+num_attempts = 1000
 
 def install(package):
     if hasattr(pip, 'main'):
@@ -19,7 +19,7 @@ try:
             from scapy.all import *
 except:
             install("scapy")
-load_layer('tls')
+
 def run_client():
     try:
         os.mkdir('../openssl/results')
@@ -27,26 +27,13 @@ def run_client():
         pass
     cipher_lst = ["round5_r5n1_1kem_0d", "round5_r5n1_3kem_0d", "round5_r5n1_5kem_0d", "round5_r5nd_1kem_0d", "round5_r5nd_3kem_0d", "round5_r5nd_3kem_5d", "round5_r5nd_5kem_0d", "round5_r5nd_5kem_5d"]
     for cipher in cipher_lst:
-        open('../openssl/results/{}.pcap'.format(cipher), 'a').close()
-        record = open('../openssl/results/test.pcap', 'rb').read()
-        record1 = TLS(record)
-        for rec in record1:
-            sess = rec.tls_session
-            rec.show()
-
-        # t = AsyncSniffer(prn=lambda x:x.summary(), filter="tcp")#lfilter=lambda x: TLS in x)
-        # t.start()
-        #tcpdump = subprocess.Popen(['tcpdump', '--time-stamp-precision', 'nano', '-i', 'eth0', '-w', '../openssl/results/{}.pcap'.format(cipher)], stdout=subprocess.PIPE)
-        #time.sleep(2)
+        tcpdump = subprocess.Popen(['tcpdump', '--time-stamp-precision', 'nano', '-i', 'eth0', '-w', '../openssl/results/{}.pcap'.format(cipher)], stdout=subprocess.PIPE)
+        time.sleep(2)
         print("Running measurements...")
         for i in range(num_attempts):
             os.system('echo hi | ../openssl/apps/openssl s_client -connect server:4433 -curves {} -tls1_3'.format(cipher))
-        break
-        #os.system("tshark -F pcap -r " + cipher + ".pcap"  + " -w " + cipher + ".pcap")
-        # t.stop()
-        # print(t.results)
-        #t.summary()[2].show()
-        #tcpdump.terminate()
+        time.sleep(5)
+        tcpdump.terminate()
 
 def main():
     run_client()
