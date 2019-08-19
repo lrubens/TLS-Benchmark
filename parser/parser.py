23b88ee96b45803e2d7a375a4c87d321f7143498
lrubens <rubenslacouture@gmail.com>
2019-08-19 19:04:43 +0000

    testing new chart

--- parser/parser.py
+++ parser/parser.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 import matplotlib.pyplot as plt
-cipher_list = ["round5_r5n1_1kem_0d", "round5_r5n1_3kem_0d", "round5_r5n1_5kem_0d", "round5_r5nd_1kem_0d", "round5_r5nd_3kem_0d", "round5_r5nd_3kem_5d", "round5_r5nd_5kem_0d", "round5_r5nd_5kem_5d"]
+cipher_list = ['round5_r5nd_5ccakem_5d', 'round5_r5nd_5kem_0d', 'round5_r5nd_1kem_0d', 'round5_r5nd_0kem_2iot', 'round5_r5nd_1kem_4longkey', 'round5_r5n1_3ccakem_0d', 'round5_r5nd_5ccakem_0d', 'round5_r5n1_5kem_0d', 'round5_r5n1_1kem_0d', 'round5_r5n1_3kem_0d', 'round5_r5nd_1kem_5d', 'round5_r5nd_5kem_5d', 'round5_r5nd_3kem_0d', 'round5_r5nd_3kem_5d', 'round5_r5nd_3ccakem_5d', 'round5_r5n1_5ccakem_0d', 'round5_r5nd_1ccakem_0d', 'round5_r5nd_1ccakem_5d', 'round5_r5n1_1ccakem_0d', 'round5_r5nd_3ccakem_0d']
+name_list = ['r5nd_5ccakem_5d', 'r5nd_5kem_0d', 'r5nd_1kem_0d', 'r5nd_0kem_2iot', 'r5nd_1kem_4longkey', 'r5n1_3ccakem_0d', 'r5nd_5ccakem_0d', 'r5n1_5kem_0d', 'r5n1_1kem_0d', 'r5n1_3kem_0d', 'r5nd_1kem_5d', 'r5nd_5kem_5d', 'r5nd_3kem_0d', 'r5nd_3kem_5d', 'r5nd_3ccakem_5d', 'r5n1_5ccakem_0d', 'r5nd_1ccakem_0d', 'r5nd_1ccakem_5d', 'r5n1_1ccakem_0d', 'r5nd_3ccakem_0d']
 avgList = list()
 double_fin = False
 for cipher in cipher_list:
@@ -26,10 +27,11 @@ for cipher in cipher_list:
         for i in range(len(synList)):
             sum += finList[i] - synList[i]
         avg = sum/len(synList)
-        avgList.append(avg)
+        avgList.append(avg * 1000)
     print(cipher + ': ' + str(avg))
-plt.bar(cipher_list, avgList)
-plt.xticks(rotation=45)
+plt.bar(name_list, avgList)
+plt.ylabel("milliseconds")
+plt.xticks(rotation=90)
 plt.tight_layout()
-plt.savefig("graph.png")
+plt.savefig("graph2.png")
 plt.clf()
--- scripts/client.py
+++ scripts/client.py
@@ -7,7 +7,7 @@ import shutil
 import time
 import pip
 
-num_attempts = 1000
+num_attempts = 200
 
 def install(package):
     if hasattr(pip, 'main'):
@@ -25,7 +25,7 @@ def run_client():
         os.mkdir('../openssl/results')
     except:
         pass
-    cipher_lst = ["round5_r5n1_1kem_0d", "round5_r5n1_3kem_0d", "round5_r5n1_5kem_0d", "round5_r5nd_1kem_0d", "round5_r5nd_3kem_0d", "round5_r5nd_3kem_5d", "round5_r5nd_5kem_0d", "round5_r5nd_5kem_5d"]
+    cipher_lst = ['round5_r5nd_5ccakem_5d', 'round5_r5nd_5kem_0d', 'round5_r5nd_1kem_0d', 'round5_r5nd_0kem_2iot', 'round5_r5nd_1kem_4longkey', 'round5_r5n1_3ccakem_0d', 'round5_r5nd_5ccakem_0d', 'round5_r5n1_5kem_0d', 'round5_r5n1_1kem_0d', 'round5_r5n1_3kem_0d', 'round5_r5nd_1kem_5d', 'round5_r5nd_5kem_5d', 'round5_r5nd_3kem_0d', 'round5_r5nd_3kem_5d', 'round5_r5nd_3ccakem_5d', 'round5_r5n1_5ccakem_0d', 'round5_r5nd_1ccakem_0d', 'round5_r5nd_1ccakem_5d', 'round5_r5n1_1ccakem_0d', 'round5_r5nd_3ccakem_0d']
     for cipher in cipher_lst:
         tcpdump = subprocess.Popen(['tcpdump', '--time-stamp-precision', 'nano', '-i', 'eth0', '-w', '../openssl/results/{}.pcap'.format(cipher)], stdout=subprocess.PIPE)
         time.sleep(2)
